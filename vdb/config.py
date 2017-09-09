18d678db34f38dcb0a634dddc5cd1e1ca472eadc
paul <paul@cueb.io>
2017-09-09 05:19:14 +0000

    fix PS

--- vdb/config.py
+++ vdb/config.py
@@ -2,4 +2,4 @@ import os
 import multiprocessing as mp
 
 THREAD_POOL = os.environ.get('TP', 'SPK')
-POOL_SIZE = os.environ.get('PS', mp.cpu_count() // 2)
+POOL_SIZE = int(os.environ.get('PS', mp.cpu_count() // 2))
